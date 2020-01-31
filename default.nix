2a96f80f8d40d7c9de88a008df9791d484a6be54
czerwonamaupa <czerwonamaupa@protonmail.com>
2020-01-31 17:50:33 +0100

    Pin nixpkgs.

--- default.nix
+++ default.nix
@@ -1,15 +1,29 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import ./nixpkgs.nix }:
 with pkgs;
 let
   overrides = self: super: {
     doctemplates = self.doctemplates_0_5;
+    pandoc = haskell.lib.justStaticExecutables
+      (self.callCabal2nix "pandoc" (nix-gitignore.gitignoreSource [] ./.) {});
   };
 
-  source-overrides = {
+  haskellPackages = haskell.packages.ghc865.override { inherit overrides; };
+
+  drv = haskellPackages.pandoc;
+
+  shell = haskellPackages.shellFor {
+    withHoogle = true;
+    packages = p: [ p.pandoc ];
+    buildInputs = with haskellPackages; [
+      cabal-install
+      apply-refact
+      hindent
+      hlint
+      stylish-haskell
+      hasktags
+      hoogle
+      (import (builtins.fetchTarball "https://github.com/hercules-ci/ghcide-nix/tarball/master") {}).ghcide-ghc865
+    ];
   };
 
-in (haskellPackages.extend (
-  lib.composeExtensions
-    (haskellPackages.packageSourceOverrides source-overrides)
-    overrides)
-).callCabal2nix "pandoc" (nix-gitignore.gitignoreSource [] ./.) {}
+in drv // { inherit shell; }
---
+++ nixpkgs.nix
@@ -0,0 +1,4 @@
+import (fetchTarball {
+  url = "https://github.com/NixOS/nixpkgs/archive/d00582b437924da25fa3699383b918985dbc7641.tar.gz";
+  sha256 = "1hq4jvbsk3fdir3qasgx19qyh6nlybbglkbr8ck78ka6h5ni13yq";
+}) {}
